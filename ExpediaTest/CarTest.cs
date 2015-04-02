12a0049b42d75f53881e6e112a6a68795b8a4294
Ethan Campbell <campbeeg@rose-hulman.edu>
2015-04-02 01:49:44 -0400

    Finished and all tests passing

--- ExpediaTest/CarTest.cs
+++ ExpediaTest/CarTest.cs
@@ -50,5 +50,69 @@ namespace ExpediaTest
 		{
 			new Car(-5);
 		}
+
+        [TestMethod()]
+        public void TestThatCarDoesGetCarLocationFromTheDatabase()
+        {
+            IDatabase mockDB = mocks.DynamicMock<IDatabase>();
+
+            String carLocation = "Hotel";
+            String anotherCarLocation = "Parking Garage";
+
+            using (mocks.Ordered())
+            {
+                Expect.Call(mockDB.getCarLocation(1025)).Return(anotherCarLocation);
+                Expect.Call(mockDB.getCarLocation(24)).Return(carLocation);
+            }
+
+            mockDB.Stub(x => x.getCarLocation(Arg<int>.Is.Anything)).Return("No car");
+
+            mocks.ReplayAll();
+
+            Car target = new Car(10);
+            target.Database = mockDB;
+            String result;
+
+            result = target.getCarLocation(1025);
+            Assert.AreEqual(anotherCarLocation, result);
+
+            result = target.getCarLocation(24);
+            Assert.AreEqual(carLocation, result);
+
+            result = target.getCarLocation(25);
+            Assert.AreEqual("No car", result);
+
+            mocks.VerifyAll();
+        }
+
+        [TestMethod()]
+        public void TestThatCarDoesGetMileageFromDatabase()
+        {
+            IDatabase mockDatabase = mocks.StrictMock<IDatabase>();
+            int Miles = 30;
+
+            Expect.Call(mockDatabase.Miles).PropertyBehavior();
+
+            mocks.ReplayAll();
+
+            mockDatabase.Miles = Miles;
+
+            var target = new Car(10);
+            target.Database = mockDatabase;
+
+            int mileage = target.Mileage;
+            Assert.AreEqual(mileage, Miles);
+
+            mocks.VerifyAll();
+        }
+
+        [TestMethod()]
+        public void TestThatCarReturnsName()
+        {
+            String expectedName = "BMW M5 Coupe";
+            var target = ObjectMother.BMW();
+            String actualName = target.Name;
+            Assert.AreEqual(expectedName, actualName);
+        }
 	}
 }
